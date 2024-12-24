e9768c6a69c557fbb524c241f95fbee64bd22459
paralaif <paralaif.xox@gmail.com>
2024-12-23 20:30:14 -0600

    Update provisioning_script.sh

--- provisioning_script.sh
+++ provisioning_script.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo "Creando ambiente virtual"
-cd /network/ComfyUI/
-python -m venv comfyuienv
-source comfyuienv/bin/activate
-echo "Ambiente virtual creado"
+!pip install sageattention
